1786fec1c97ab12b701c0914534d3818c318bead
Dmitry Martynov <dmitry.martynov@stash.com>
2022-07-05 13:54:56 +0100

    initial conf

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -gx SBT_OPTS -Xms4096M -Xmx4096M -Xss4M
+    set -gx SBT_CREDENTIALSset -gx SBT_CREDENTIALS
+    set fzf_fd_opts --hidden --exclude=.git
+end
+
+# test
+
