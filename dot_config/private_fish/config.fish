a5258ee30ec8fee5411ba8b39a91bca5bd3fc5bd
Dmitry Martynov <dmitry.martynov@stash.com>
2023-10-22 07:12:40 -0400

    add some macos config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx SBT_OPTS -Xms4096M -Xmx4096M -Xss4M
-    set -gx SBT_CREDENTIALSset -gx SBT_CREDENTIALS
+    set -gx SBT_CREDENTIALS
     set fzf_fd_opts --hidden --exclude=.git
 end
 
+set -gx VISUAL nvim
 set -gx EDITOR nvim
-
 
